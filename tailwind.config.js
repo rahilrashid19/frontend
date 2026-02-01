76f0dc5b15cd2dc2ac3628ce98d7a24a87af57c8
Rahil Rashid <rahilrashid977@gmail.com>
2026-02-01 12:51:47 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("tailwindcss-animate")],
+};
